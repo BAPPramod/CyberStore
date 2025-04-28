1e446bcc22b5227820cf985c0fa1c902cecbc095
Pramod <bappramod@gmail.com>
2025-04-29 01:26:14 +0300

    Finalized 2 for deployment

--- src/main/java/fi/haagahelia/cyberstore/config/SecurityConfig.java
+++ src/main/java/fi/haagahelia/cyberstore/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -30,6 +31,8 @@ public class SecurityConfig {
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 System.out.println("Initializing SecurityFilterChain");
                 http
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                                 .authorizeHttpRequests(authz -> {
                                         System.out.println("Configuring authorization rules");
                                         authz.requestMatchers("/", "/signup", "/css/**", "/js/**", "/images/**",
